ba18bca5a85aaffbb5a2e24449be65a246611afb
Ramil' Osmanov <osmanovramil823@gmail.com>
2023-11-02 23:43:11 +0600

    negative card styling

--- client/src/components/Negative/NegativeCard.tsx
+++ client/src/components/Negative/NegativeCard.tsx
@@ -1,9 +1,14 @@
+import styles from "./NegativeCard.module.css";
 import { Link } from "react-router-dom";
 
 export const NegativeCard = () => {
   return (
-    <div>
-      <Link to='/negative'>Negative card</Link>
-    </div>
+    <Link to='/negative' className={styles.link}>
+      <div className={styles.card}>
+        <div className={styles.card__wrapper}>
+          <div className={styles.card__text}>NEGATIVE</div>
+        </div>
+      </div>
+    </Link>
   );
 };
