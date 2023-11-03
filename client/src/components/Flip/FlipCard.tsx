f8b06c7f19eff295548e87ac03ddd4649c42b1e7
Andrew Chernigovsky <chernigovsky108@gmail.com>
2023-11-03 13:31:27 +0300

    flipCard

--- client/src/components/Flip/FlipCard.tsx
+++ client/src/components/Flip/FlipCard.tsx
@@ -1,9 +1,42 @@
+import styles from "./FlipCard.module.css";
 import { Link } from "react-router-dom";
 
-export const FlipCard = () => {
+type Props = {
+  flipX: boolean;
+  flipY?: boolean | undefined;
+};
+
+export const FlipCard = ({ flipX = true, flipY = false }: Props) => {
+  let text;
+  let flipEffects;
+
+  if (flipX) {
+    text = "FLIP-X";
+    flipEffects = styles.cardX;
+  }
+  if (flipY) {
+    text = "FLIP-Y";
+    flipEffects = styles.cardY;
+  }
+
+  if (flipX && flipY) {
+    text = "FLIP-XY";
+    flipEffects = styles.cardXY;
+  }
+
+  const FLIP = (text?: string, flipEffects?: string) => {
+    return (
+      <div className={`${styles.card} ${flipEffects}`}>
+        <div className={styles.card__wrapper}>
+          <div className={styles.card__text}>{text}</div>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <Link to={"/flip"}>flip card</Link>
-    </div>
+    <Link to='/flip' className={styles.link}>
+      {FLIP(text, flipEffects)}
+    </Link>
   );
 };
