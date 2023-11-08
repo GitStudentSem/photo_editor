3a31103256719eff468623169a15ce2cb55c1342
Ramil' Osmanov <osmanovramil823@gmail.com>
2023-11-09 02:59:32 +0600

    photo processing on the server (negative)

--- client/src/components/Negative/NegativePage.tsx
+++ client/src/components/Negative/NegativePage.tsx
@@ -3,6 +3,7 @@ import styles from "./NegativePage.module.css";
 
 export const NegativePage = () => {
   const [photo, setPhoto] = useState<string>();
+  const [isAlpha, setIsAlpha] = useState(false);
   const [submitStatus, setSubmitStatus] = useState<string>();
 
   function onChange(e: React.ChangeEvent<HTMLInputElement>): void {
@@ -11,10 +12,26 @@ export const NegativePage = () => {
     }
   }
 
+  async function getEditedPhoto() {
+    console.log("function is working...", photo, typeof photo);
+
+    const res = await fetch("http://localhost:3333/negative", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        photo,
+        isAlpha: false,
+      }),
+    }).then(res => res.json()).then(data => console.log(data));
+  }
+
   function onSubmit(e: FormEvent<HTMLButtonElement>) {
     if (photo) {
       e.preventDefault();
       setSubmitStatus("Обрабатываем ваше фото. Подождите...");
+      getEditedPhoto()
       //another function do post request to the controller
     } else {
       setSubmitStatus("Загрузите фото перед отправкой");
@@ -22,39 +39,35 @@ export const NegativePage = () => {
     }
   }
 
-  // async function getEditedPhoto() {
-  //   const photo = await fetch('http://localhost:3333/negative', {method: 'POST', body: JSON.stringify({msg: 'photo'})});
-  // }
-
   return (
     <div className={styles.negativePage}>
       <div className={styles.negativePage__wrapper}>
         <div className={styles.photo}>
           <div className={styles.photo__wrapper}>
-            <img src={photo} alt='Фото' />
+            <img src={photo} alt="Фото" />
           </div>
         </div>
         <div className={styles.settings}>
-          <input type='file' onChange={onChange} />
+          <input type="file" onChange={onChange} />
           <div className={styles.settings__item}>
             <label>
               Настройка n
-              <input type='text' placeholder='Какая то настройка!' />
+              <input type="text" placeholder="Какая то настройка!" />
             </label>
           </div>
           <div className={styles.settings__item}>
             <label>
               Настройка n
-              <input type='text' placeholder='Какая то настройка!' />
+              <input type="text" placeholder="Какая то настройка!" />
             </label>
           </div>
           <div className={styles.settings__item}>
             <label>
               Настройка n
-              <input type='text' placeholder='Какая то настройка!' />
+              <input type="text" placeholder="Какая то настройка!" />
             </label>
           </div>
-          <button type='submit' onClick={onSubmit}>
+          <button type="submit" onClick={onSubmit}>
             Отправить на обработку
           </button>
           <p>{submitStatus}</p>
--- server/controllers/negativeController.js
+++ server/controllers/negativeController.js
@@ -1,8 +1,26 @@
 import { sendError } from "../assets.js";
+import sharp from "sharp";
+import * as fs from "fs";
 
 export const negative = async (req, res) => {
   try {
+    const { photo, isAlpha } = req.body;
+
+    const output = await sharp(photo)
+      .negate({ alpha: false });
+
+    const blob = new Blob([output], { type: "image/png" });
+    const editedImage = URL.createObjectURL(blob);
+    fs.writeFile("../client/src/image.txt", editedImage, err => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Файл создан");
+      }
+    });
+
+    return res.json({ editedImage });
   } catch (error) {
-    sendError({ message: "Не удалось mirror", error, res });
+    sendError({ message: "Не удалось negate", error, res });
   }
 };
