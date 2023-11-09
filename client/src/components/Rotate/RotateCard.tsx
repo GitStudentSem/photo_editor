62404f765245ef70ab3e0094c60a97d576fda30f
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2023-11-09 09:35:36 +0300

    styles for rotate card

--- client/src/components/Rotate/RotateCard.tsx
+++ client/src/components/Rotate/RotateCard.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import s from "./rotateCard.module.css";
 
 export const RotateCard = () => {
   return (
-    <div>
-      <Link to='/rotate'>Rotate card</Link>
-    </div>
+    <Link className={s.wrapper} to='/rotate'>
+      <span className={s.text}>Rotate</span>
+    </Link>
   );
 };
--- client/src/components/Rotate/RotatePage.tsx
+++ client/src/components/Rotate/RotatePage.tsx
@@ -1,9 +1,10 @@
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent, FormEvent, useRef } from "react";
 import s from "./style.module.css";
 import { UploadControl } from "./UploadControl";
 
 const RotatePage = () => {
-  const [image, setImage] = useState("");
+  const filePickerRef = useRef<HTMLInputElement>(null);
+  const [image, setImage] = useState<any>(""); // TODO Определить тип картинки, заменить any
   const [angle, setAngle] = useState(0);
   const [background, setBackground] = useState("red");
 
@@ -12,22 +13,28 @@ const RotatePage = () => {
       setImage(URL.createObjectURL(e.target.files[0]));
     }
   };
-  const onSend = async (e: FormEvent<HTMLButtonElement> | undefined) => {
-    if (!e) return;
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("image", image);
-    formData.append("angle", angle.toString());
-    formData.append("background", background);
 
-    const response = await fetch("http://localhost:3333/rotate", {
-      method: "POST",
-      //   headers: {
-      //     "Content-Type": "multipart/form-data",
-      //   },
-      body: formData,
-    });
-    console.log("res", response);
+  const onSend = async (e: FormEvent<HTMLButtonElement> | undefined) => {
+    try {
+      if (!e) return;
+      e.preventDefault();
+      const formData = new FormData();
+
+      formData.append("image", image);
+      //   formData.append("angle", angle.toString());
+      //   formData.append("background", background);
+
+      const response = await fetch("http://localhost:3333/rotate", {
+        method: "POST",
+        // headers: {
+        //   "Content-Type": "multipart/form-data",
+        // },
+        body: formData,
+      });
+      console.log("res", response);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -43,10 +50,27 @@ const RotatePage = () => {
       </div>
       <div className={s.controls_wrapper}>
         <div className={s.input_wrapper}>
+          <button
+            onClick={() => {
+              if (!filePickerRef.current) return;
+              filePickerRef.current.click();
+            }}
+          >
+            Загрузить изображение
+          </button>
+          <input
+            ref={filePickerRef}
+            type='file'
+            hidden
+            accept='.png,.jpeg'
+            onChange={onImageChange}
+          />
+        </div>
+        {/* <div className={s.input_wrapper}>
           <UploadControl onChange={onImageChange} accept='.png,.jpeg'>
             Выбрать фотографию
           </UploadControl>
-        </div>
+        </div> */}
 
         <label className={s.input_wrapper}>
           <p>На какой угол нужно повернуть изображение?</p>
