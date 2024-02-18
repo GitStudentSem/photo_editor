8bdb853951564c7e5253e95c0e79f20eadd1ec86
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2024-02-18 16:40:00 +0300

    fix render controls

--- client/src/components/Rotate/rotatePage/RotatePage.tsx
+++ client/src/components/Rotate/rotatePage/RotatePage.tsx
@@ -1,29 +1,14 @@
-import { Fragment } from "react";
-import { TextInput } from "../../textInput/TextInput";
+import { createElement } from "react";
 
-import { Checkbox } from "../../checkbox/Checkbox";
-import { ColorInput } from "../../colorInput/ColorInput";
 import { observer } from "mobx-react-lite";
 import { Layout } from "../../layout/Layout";
 import rotateStore from "../../../store/rotateStore";
 
 const RotatePage = observer(() => {
-  const enumTags = {
-    TextInput,
-    Checkbox,
-    ColorInput,
-  };
-
   return (
     <Layout>
       {rotateStore.controls.map(({ name, props }) => {
-        const Control = enumTags[name];
-
-        return (
-          <Fragment key={name}>
-            {Reflect.apply(Control, null, [props])}
-          </Fragment>
-        );
+        return createElement(name, { ...props, key: name });
       })}
     </Layout>
   );
